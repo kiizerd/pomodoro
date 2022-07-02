2f467e35db8533cf1cffafdd049483eebf4fa28c
kiizerd <ajsewell13@gmail.com>
2022-07-01 22:50:03 -0500

    Added ButtonGroup component to group Buttons.

---
+++ src/components/ButtonGroup.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const StyledButtons = styled.section`
+  margin: 10px;
+  padding: 8px 4px;
+  display: flex;
+  }
+`;
+
+type Props = {
+  children: Array<JSX.Element>;
+};
+
+const ButtonGroup = ({ children }: Props) => {
+  return (
+    <StyledButtons>
+      {children}
+    </StyledButtons>
+  );
+};
+
+export default ButtonGroup;
